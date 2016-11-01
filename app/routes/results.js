864a5cb5b2ddd8092f1d20d6c94faa7f9cf338cc
Alvaro Flores and Garrett Boggs <Guest@epicodus-94.local>
2016-11-01 11:01:16 -0700

    add committees to results page

--- app/routes/results.js
+++ app/routes/results.js
@@ -2,11 +2,20 @@ import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
+  model(params) {
     var key = config.myApiKey;
     var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +key+ '&zip=' +params.zip;
-    return Ember.$.getJSON(url).then(function(responseJSON) {
-      return responseJSON.results;
+    var url2 = 'http://congress.api.sunlightfoundation.com/committees?apikey=' +key;
+
+    return Ember.RSVP.hash({
+      legislators: Ember.$.getJSON(url).then(function(responseJSON) {
+        console.log(responseJSON.results);
+        return responseJSON.results;
+      }),
+      committees: Ember.$.getJSON(url2).then(function(responseJSON) {
+        console.log(responseJSON.results);
+        return responseJSON.results;
+      })
     });
   }
 });
