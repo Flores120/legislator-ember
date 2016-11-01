ba26f5e2b3721bad5ef936d4175fe1f271bf4ecd
Alvaro Flores and Garrett Boggs <Guest@epicodus-94.local>
2016-11-01 13:39:34 -0700

    added committees

--- app/routes/results.js
+++ app/routes/results.js
@@ -4,8 +4,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     var key = config.myApiKey;
+    console.log(params.chamber + " Chamber value") ;
+    console.log(params.zip + " Zip Code value") ;
     var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +key+ '&zip=' +params.zip;
-    var url2 = 'http://congress.api.sunlightfoundation.com/committees?apikey=' +key;
+    var url2 = 'http://congress.api.sunlightfoundation.com/committees?chamber=house&subcommittee=false&apikey=' +key;
+    var url3 = 'http://congress.api.sunlightfoundation.com/committees?chamber=senate&subcommittee=false&apikey=' +key;
 
     return Ember.RSVP.hash({
       legislators: Ember.$.getJSON(url).then(function(responseJSON) {
@@ -15,7 +18,11 @@ export default Ember.Route.extend({
       committees: Ember.$.getJSON(url2).then(function(responseJSON) {
         console.log(responseJSON.results);
         return responseJSON.results;
-      })
+      }),
+      senateCommittees: Ember.$.getJSON(url3).then(function(responseJSON) {
+        console.log(responseJSON.results);
+        return responseJSON.results;
+      }),
     });
   }
 });
